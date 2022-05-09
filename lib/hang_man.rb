cc1991659d3a951b96f25a5a5127dbaabc52093c
lkatchutas-99 <96667853+lkatchutas-99@users.noreply.github.com>
2022-05-08 22:49:56 -0400

    Complete hangman and add load and save functionality
    - Completed main game
    - removed redundant methods or operations.
    - Globalized methods that do not interact with instance variables
    - added load and save features

--- lib/hang_man.rb
+++ lib/hang_man.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: false
 
+require 'ostruct'
 require 'json'
 require 'pry-byebug'
 
@@ -8,18 +9,61 @@ def extract_words
   words = []
   until file.eof?
     word = file.readline
-    words.push(word) if choice_in_range?(word.length, 13, 6)
+    words.push(word.gsub("\n", '')) if choice_in_range?(word.length, 13, 6)
   end
   file.close
   words
 end
 
 def load_game
-
+  if Dir.exist?('saved_games') && !Dir.empty?('saved_games')
+    load_file
+  else
+    puts 'There are no saved games, a new game has been started '
+    Game.new
+  end
 end
 
-def save_game(game)
+def load_file
+  file = options(Dir.children('./saved_games'), 'Select a file to load')
+  Game.json_create(JSON.parse(File.read("./saved_games/#{file}")))
+  puts 'Resuming from previous session'
+end
 
+def save_game(data)
+  Dir.mkdir('saved_games') unless Dir.exist?('saved_games')
+  prompt('Choose a file name')
+  file_name = prompt_file_name
+  write_to_file(file_name, data)
+end
+
+def write_to_file(file_name, data)
+  file_name = overwrite(file_name)
+  File.open("./saved_games/#{file_name}.json", 'w') do |file|
+    File.truncate(file, 0)
+    file.puts data
+    puts "\"#{file_name}\" has been saved"
+  end
+end
+
+def file_exists?(file_name)
+  Dir.children('./saved_games').any? do |saved_files|
+    saved_files.gsub('.json', '').eql?(file_name)
+  end
+end
+
+def overwrite(file_name)
+  return file_name unless file_exists?(file_name)
+
+  new_file = file_name
+  if options(%w[Yes No], 'This file already exists, want to overwrite it?').eql?('No')
+    new_file += "[#{Dir.children('./saved_games').length}]"
+  end
+  new_file
+end
+
+def prompt_file_name
+  gets.chomp.gsub(' ', '_')
 end
 
 def count_down(message, custom_interval = 3, unique_char = nil)
@@ -33,21 +77,21 @@ def count_down(message, custom_interval = 3, unique_char = nil)
   puts ''
 end
 
-def display_options(option_list, prompt)
+def display_options(option_list)
   formatted_options = ''
   option_list.each_with_index { |option, idx| formatted_options += "#{idx + 1}: #{option}   " }
   formatted_options
 end
 
 def prompt_option(option_list, prompt)
-  print "\n#{prompt} "
+  print "\n#{prompt} ("
   option_list.each_with_index do |option, idx|
     print option
     unless (option_list.length - 1).eql?(idx)
-      print option_list.length.eql?(idx + 2) ? ' and ' : ', '
+      print option_list.length.eql?(idx + 2) ? ' or ' : ', '
     end
   end
-  puts ''
+  puts ')'
 end
 
 def prompt(message)
@@ -70,6 +114,7 @@ end
 
 def condition?(choice, option_list)
   return true if choice_is_number?(choice) && choice_in_range?(choice.to_i, option_list.length)
+
   error_message(choice_is_number?(choice) ? 'out of range' : 'must input number') unless choice.nil?
 end
 
@@ -86,171 +131,301 @@ def choice_is_number?(choice)
 end
 
 def error_message(context)
-  prompt(("Error, #{context}, please try again")) if context
+  prompt("Error, #{context}, please try again") if context
+end
+
+def get_setter(player)
+  player.eql?('Human') ? Human.new : Computer.new
+end
+
+def letter_found?(guessed_letter, correct_letter)
+  correct_letter['correct_letter'].eql?(guessed_letter) && correct_letter['found'].eql?(false)
+end
+
+def save_name
+  prompt('Please enter a name for this save')
 end
 
 # Game
 class Game
-  def initialize
-    init_players
-    init_words
-    init_columns
-    init_board
+  attr_reader :board, :setter, :guesser, :guessed_word
+
+  def initialize(setter, guesser, board, guessed_word)
+    @setter = setter
+    @guesser = guesser
+    @board = board
+    @guessed_word = guessed_word
+    init_players if setter.nil?
+  end
+
+  def to_json(*keys)
+    {
+      JSON.create_id => self.class.name,
+      data: {
+        setter: @setter.data,
+        guesser: @guesser.data,
+        board: @board.data,
+        guessed_word: @guessed_word
+      }
+    }.to_json(*keys)
+  end
+
+  def self.json_create(data)
+    setter = get_setter(data['data']['setter']['player_type'])
+    guesser = Human.new('guesser')
+    board = Board.new(data['data']['board']['columns'])
+    setter.load_data(data['data']['setter'])
+    guesser.load_data(data['data']['guesser'])
+    guessed_word = data['data']['guessed_word']
+    new(setter, guesser, board, guessed_word)
   end
 
   def init_players
-    player = options(['Human', 'Computer'], 'What should be the setter?')
+    player = options(%w[Human Computer], 'What should be the setter?')
     create_players(player)
+    @setter.player_name
+    @guesser.player_name
   end
 
   def create_players(player)
-    @setter = (player.eql('Human') ? Human.new('setter') : Computer.new('setter'))
+    @setter = get_setter(player)
     @guesser = Human.new('guesser')
   end
 
-  def init_board
-    @board = Board.new(@columns)
+  def init_board(columns)
+    @board = Board.new(columns)
+    @board.put_board(6, [])
   end
 
   def init_columns
-    @columns = @current_word.length
-    @guessed_word = Array.new(@columns)
+    @setter.setup_word
+    puts 'This is what the board looks like when the whole character is present'
+    init_board(@setter.word.length)
+    @guessed_word = Array.new(@setter.word.length)
   end
 
   def play_game
-    @setter.set_correct_word
-    until @board.wrong_tries.eql?(7) || @guessed_word.none { |letter| letter.nil? }
-      letter = @guesser.guess_letter
-      return if letter.eql?('quit')
-      if @setter.word.include?(letter)
-        add_correct_letter(letter)
-      else
-        @board.guess_was_wrong
-      end
-      @board.put_board
+    init_columns if @guesser.wrong_tries.eql?(0)
+    until @guesser.wrong_tries.eql?(7) || @guessed_word.none?(&:nil?)
+      return 'quit' if evaluate_guessed_letter.eql?('quit')
     end
+    evaluate_winner(@guesser.wrong_tries)
+    @guesser.wrong_tries = 0
+    @guesser.chosen_letters = []
   end
 
-  def add_correct_letter(letter)
-    
+  def evaluate_guessed_letter
+    letter = enter_letter
+    return letter if letter.eql?('quit')
+
+    @guesser.chosen_letters.push(letter) unless letter.eql?('save')
+    unless @setter.word.find { |correct_letter| letter_found?(letter, correct_letter) }
+      letter = @guesser.guess_was_wrong
+    end
+    add_correct_letter(letter)
+    @board.put_board(@guesser.wrong_tries, @guesser.chosen_letters, @guessed_word)
+  end
+
+  def enter_letter
+    letter = @guesser.guess_letter(@board, @guessed_word)
+    while letter.eql?('save')
+      save_game(to_json)
+      @board.put_board(@guesser.wrong_tries, @guesser.chosen_letters, @guessed_word)
+      letter = @guesser.guess_letter(@board, @guessed_word)
+    end
+    letter
+  end
+
+  def evaluate_winner(wrong_tries)
+    wrong_tries.eql?(7) ? @setter.winner(@setter.word) : @guesser.winner(@setter.word)
+    @board.show_results(@setter, @guesser)
+  end
+
+  def add_correct_letter(guessed_letter)
+    return if guessed_letter.nil?
+
+    @setter.word.each_with_index do |correct_letter, idx|
+      if letter_found?(guessed_letter, correct_letter)
+        @guessed_word[idx] = guessed_letter
+        @setter.found_letter(idx)
+      end
+    end
   end
 end
 
+# Player
 class Player
-  attr_reader :score, :word
-  def initialize(player_type, player_mode)
+  attr_reader :score, :word, :name
+  attr_accessor :wrong_tries, :chosen_letters
+
+  def initialize(player_type, player_mode, name = '', score = 0, wrong_tries = 0)
     @player_type = player_type
     @player_mode = player_mode
-    @score = 0
-    @wrong_tries = 0
+    @score = score
+    @chosen_letters = []
+    @wrong_tries = wrong_tries
+    @name = name
   end
 
-  def winner(board)
-    print "Congrats, #{@player_name}#{ 'the guesser could not find the word.' if player_mode.eql?('setter') }"
+  def data
+    {
+      player_name: @name,
+      player_type: @player_type,
+      player_mode: @player_mode,
+      score: @score,
+      wrong_tries: @wrong_tries,
+      correct_word: @word,
+      chosen_letters: @chosen_letters
+    }
+  end
+
+  def load_data(data)
+    @name = data['player_name']
+    @score = data['score']
+    @wrong_tries = data['wrong_tries']
+    @word = data['correct_word']
+    @chosen_letters = data['chosen_letters']
+  end
+
+  def setup_word(word)
+    @word = word.split('').each_with_object([]) do |arr, letter|
+      arr.push({ 'correct_letter' => letter, 'found' => false })
+    end
+  end
+
+  def found_letter(idx)
+    @word[idx]['found'] = true
+  end
+
+  def player_name(name)
+    @name = name.capitalize
+    puts "\nWelcome to hangman #{@name}, you will be #{@player_mode}"
+    puts ''
+  end
+
+  def guess_was_wrong
+    @wrong_tries += 1
+    nil
+  end
+
+  def joined_word(word_arr)
+    word_arr.reduce('') do |str, letter|
+      str += letter['correct_letter']
+      str
+    end
+  end
+
+  def winner(word_arr)
+    puts "End of game, word was #{joined_word(word_arr)}"
+    print "Congrats #{@name}#{', the guesser could not find the word.' if @player_mode.eql?('setter')}"
     puts ' You win and you gained a point'
     @score += 1
   end
 end
 
+# Human
 class Human < Player
-  def initialize(player_mode)
+  def initialize(player_mode = 'setter')
     super('Human', player_mode)
   end
 
   def player_name
-    return @player_name if @player_name
-
-    prompt('Human, please enter your name')
-    @player_name = gets.chomp
+    prompt("Human (#{@player_mode}), please enter your name")
+    super(gets.chomp)
   end
 
-  def guess_was_wrong
-    @wrong_tries += 1
-  end
-
-  def guess_letter
-    prompt('Choose a (1) character to guess the code, type a single "quit" to quit (case insensitive')
+  def guess_letter(board, guessed_word)
+    prompt("#{@name}, Choose a (1) character to guess the code, type 'save' to save current game and type 'quit' exit")
     letter = nil
-    validate_guess(letter)
-    letter
+    validate_guess(letter, board, guessed_word)
   end
 
-  def validate_guess(letter)
+  def validate_guess(letter, board, guessed_word)
     until letter && choice_in_range?(letter.length, 1)
       letter = gets.chomp
-      (are_you_sure?('quit') ? break : next) if letter.downcase.eql?('quit')
-      unless letter.nil? || choice_in_range?(letter.length, 1)
-        error_message('only one letter is accepted')
+      if letter.downcase.eql?('quit') || letter.downcase.eql?('save')
+        (are_you_sure?(letter, board, guessed_word) ? break : next)
       end
+      validation_error(letter)
     end
+    letter.downcase
   end
 
-  def set_correct_word
-    prompt('Human (setter), please enter the secret word (between 5 and 12 characters')
+  def validation_error(letter)
+    return if letter.nil? || choice_in_range?(letter.length, 1)
+
+    error_message('only one letter, "save" or "quit" is accepted')
+  end
+
+  def setup_word
+    @wrong_tries = 0
+    prompt("#{@name.capitalize} (setter), please enter the secret word (between 5 and 12 characters)")
     word = ''
     until choice_in_range?(word.length, 12, 5)
       word = gets.chomp
-      error_message('') unless choice_in_range?(word.length, 12, 5)
+      error_message('word must be between 5 and 12 characters inclusive') unless choice_in_range?(word.length, 12, 5)
     end
-    @word = word
+    super(word)
   end
 
-  def are_you_sure?(context)
-    options(['Yes', 'No'], "Are you sure you want to #{context}?").eql?('Yes')
+  def are_you_sure?(context, board, guessed_word)
+    return 'Yes' if options(%w[Yes No], "Are you sure you want to #{context}?").eql?('Yes')
+
+    board.put_board(@wrong_tries, @chosen_letters, guessed_word)
+    prompt("Choose a (1) character to guess the code, type 'save' to save current game and type 'quit' exit")
   end
 end
 
+# Computer
 class Computer < Player
-  def initialize(player_mode)
+  def initialize(player_mode = 'setter')
     super('Computer', player_mode)
   end
 
   def player_name
-    return @player_name if @player_name
-
-    count_down('Computer entering name')
-    @player_name = "Computer_#{rand(0..999)}"
+    count_down('Computer choosing name')
+    super("Computer_#{rand(0..999)}")
   end
-  
-  def set_correct_word
+
+  def setup_word
+    count_down("#{@name} setting up word")
     words = extract_words
-    @word = words[rand(0...words.length)]
-    @word.slice!(-1, 2)
-  end
-
-  def are_you_sure?(context)
-    options(['Yes', 'No'], "Are you sure you want to #{context}?").eql?('Yes')
+    super(words[rand(0...words.length)])
   end
 end
 
+# Board
 class Board
-  attr_reader :columns, :wrong_tries
+  attr_reader :columns
+
   def initialize(columns)
     @columns = columns
     @hang_man_limbs = ["F O\n", 'F/', '|', "\\\n", 'F/', ' \\']
-    puts 'This is what the board looks like'
-    put_board(6)
   end
 
-  def put_board(limbs, guessed_word = nil)
+  def data
+    {
+      columns: @columns
+    }
+  end
+
+  def put_board(limbs, chosen_letters, guessed_word = nil)
     print positioning('__')
     puts '_'
     print positioning('  ')
-    puts " \\"
-    puts hang_man_guy(limbs)
-    puts "\n"
+    puts ' \\'
+    puts hang_man_guy(limbs.eql?(7) ? 6 : limbs)
+    puts "\n\nSo far, you chose >> #{chosen_letters.join(' ')}" unless chosen_letters.empty?
+    puts "\nCorrect Letters:"
     lines(guessed_word)
   end
 
-
   def show_results(setter, guesser)
-    print positioning('  ')
-    puts 'Points:'
-    print "#{guesser.player_name} (guesser): #{guesser.player_points} "
-    print positioning('   ')
-    puts "#{setter.player_name} (setter): #{setter.player_points} "
+    puts "\nPoints:"
+    puts "#{guesser.name} (guesser): #{guesser.score} "
+    puts "#{setter.name} (setter): #{setter.score} "
   end
-  
+
   def hang_man_guy(limbs)
     guy = positioning('  ')
     guy += "( )\n"
@@ -259,18 +434,18 @@ class Board
 
   def add_limbs(guy, limbs)
     limbs.times do |limb|
-      if @hang_man_limbs[limb].include?('F')
-        @hang_man_limbs[limb].slice!(0, 1)
-        guy += positioning('  ')
-      end
-      guy += @hang_man_limbs[limb]
+      guy += if @hang_man_limbs[limb].include?('F')
+               positioning('  ') + @hang_man_limbs[limb].sub('F', '')
+             else
+               @hang_man_limbs[limb]
+             end
     end
     guy
   end
 
   def lines(word)
     puts ''
-    word.each { |letter| print "  #{letter || ' '}  " } unless word.nil?
+    word&.each { |letter| print " #{letter || ' '}   " }
     puts ''
     print positioning('---  ')
     puts ''
@@ -285,24 +460,29 @@ end
 
 puts 'Welcome to mastermind, in order to play,'
 puts 'Player must choose a letter that has been'
-puts 'Pre selected by the computer or human as the setter' 
+puts 'Pre selected by the computer or human as the setter'
 puts 'can choose whether a computer or human sets'
 puts 'the correct word. Human can set whatever they'
 puts 'want (must be between 5 and 12 characters inclusive)'
 puts 'but the computer will randomly choose their word.'
-puts 'The word guesser must find all the letters before'
-puts 'the stick-man figure fully appears. If the guesser'
+puts 'The word guesser must find all the letters until after'
+puts 'the stick-man figure fully appears (or 7 tries). If the guesser'
 puts 'wins, they earn a point, whereas the setter gains a '
-puts 'point if the guesser loses. There are no rounds, points'
+puts 'point if the guesser loses. There are no rounds, points (for each game)'
 puts 'go on forever unless the guesser (always human) removes'
 puts 'or overwrites a save. There is no character validation for the guesser,'
 puts 'but they must only type one character per round'
 
-Dir.mkdir('saved-games') unless Dir.exist?('saved-games')
+game = options(['new game', 'load existing game']).eql?('new game') ? Game.new(nil, nil, nil, nil) : load_game
 
-if options(['new game', 'load existing game']).eql?('new game')
-  game = Game.new
-else
+play_again = 'Yes'
 
+while game && play_again.eql?('Yes')
+  break if game.play_game.eql?('quit')
+
+  play_again = options(%w[Yes No], 'Want to play again?')
 end
 
+save_game(game.to_json) if options(%w[Yes No], 'Save current game?').eql?('Yes')
+
+puts "\nGoodbye!!!"
